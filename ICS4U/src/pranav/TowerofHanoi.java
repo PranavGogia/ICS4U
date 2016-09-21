e3844140afb4b457feb7693821d68eeb220fc813
342629581 <342629581@C-H6304V1.tdsb.on.ca>
2016-09-21 15:00:24 -0400

    Done

---
+++ ICS4U/src/pranav/TowerofHanoi.java
@@ -0,0 +1,47 @@
+/*Pranav Gogia
+ * September 19,2016
+ * TowerofHanoi
+ * This program takes in the number of discs and gives 
+ */
+package pranav;
+// import the scanner object
+import java.util.Scanner;
+
+public class TowerofHanoi {
+
+	/**
+	 * This is the main method
+	 * This method takes in the value that the user entered and sends it to the method solve
+	 *
+	 */
+	 public static void main(String[] args) {
+		   // tell the user to enter number of discs
+	       System.out.print("Enter number of discs: ");
+	       // initiate scanner
+	       Scanner scanner = new Scanner(System.in);
+	       // take the input
+	       int discs = scanner.nextInt();
+	       // give the method 'solve' the values discs
+	       solve(discs, "Source", "Auxiliary", "Destination");
+	   }
+	
+	
+	 /**
+		 * This method determines the number of digits and increases the counter and then sends the counter value back to the main method
+		 * @param n is entered by the user and the string source, auxiliary and destination are initiated
+		 * 
+		 */
+	public static void solve(int n, String source, String auxiliary, String destination) {
+			// if the value the user entered reaches 0 or is 0 then this if structure returns the value
+	       if (n == 0) {
+	           return;
+	       } else {
+	    	   // value(n) - 1
+	           solve(n - 1, source, destination, auxiliary);
+	           System.out.println(source + " -> " + destination);
+	           solve(n - 1, auxiliary, source, destination);
+	       }
+	   }
+
+	  
+}

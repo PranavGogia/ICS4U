c8686a873f906e23467b8bf3c2e1167828deff54
342629581 <342629581@C-H6304V1.tdsb.on.ca>
2016-09-21 14:54:47 -0400

    Done

--- ICS4U/src/pranav/RecursionExcercise1.java
+++ ICS4U/src/pranav/RecursionExcercise1.java
@@ -1,49 +1,58 @@
+/*Pranav Gogia
+ * September 15,2016
+ * Recursion Exercise 1
+ * This program takes in the number of the exponent and then prints out asterisks that are 2^number entered long
+ */
 package pranav;
-
+//import the scanner object
 import java.util.Scanner;
 
 public class RecursionExcercise1 
 {
-
+	/**
+	 * This is the main method
+	 * This method takes in the value that the user entered and sends it to the method starString
+	 *@param the value of x is given to the method starString
+	 */
 	public static void main(String[] args) 
 	{
 		
 		Scanner scn=new Scanner(System.in);
 		// take the input
-		int x = scn.nextInt();	
+		int x = 0;	
 		// print out the number of digits
-		System.out.println(starString(x));
-		scn.close();
-		
+		starString(x=Integer.parseInt(scn.nextLine()));
 		
 	}
-
-	public static int starString (int x)
+	/**
+	 * This is the method thats does all the calculations and prints the asterisks
+	 *@return the new value of x is returned
+	 *@param the value of x is given to the method starString
+	 */
+	public static void starString (int x)
 	{
+		// if x == 0 then the value is returned 
 		if (x==0)
 		{
-			return 1;
+			System.out.println("*");
+			return;
 		}
-		
-		else if (x <0)
-		{
-			System.out.println("Error");
-		
-		}
-		else
-		{
-			for (int y = 0; y<= Math.pow(2,x); y++)
-			{
+			
+		// takes the value of starString and then subtracts 1 from it to change to the stack value and calls the method again
+		starString (x-1);
+			for (int y = 0; y< Math.pow(2,x); y++)
+			
 								
 				System.out.print("*");
+				System.out.println();
 				
 				
 				
 				
-			}
 			
-		}
-		return x;
+			
+		
+	
 		
 	}
 
